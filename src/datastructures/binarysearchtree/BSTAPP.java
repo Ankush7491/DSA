121df756f3f394c9e99bae707ec470fd942e84e7
Ankush7491 <ankushsharma7491@live.com>
2024-12-22 10:11:55 +0530

    Binary Search tree classes and the insert method.

---
+++ src/datastructures/binarysearchtree/BSTAPP.java
@@ -0,0 +1,8 @@
+package datastructures.binarysearchtree;
+
+public class BSTAPP {
+    public static void main(String[] args) {
+        BinarySearchTree binarySearchTree=new BinarySearchTree();
+
+    }
+}
---
+++ src/datastructures/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,48 @@
+package datastructures.binarysearchtree;
+
+public class BinarySearchTree {
+    Node root;
+
+    class Node{
+        Node left;
+        Node right;
+        int value;
+        Node(int value){
+            this.value=value;
+        }
+    }
+    public boolean insert(int value){
+        Node toBeInserted=new Node(value);
+        if(root==null) {
+            root=toBeInserted;
+            return true;
+        }
+        Node temp=root;
+        while(temp!=null){
+            if(temp.value==toBeInserted.value) return false;
+            if(toBeInserted.value<temp.value){ //left tree
+                if(temp.left==null)
+                {
+                    temp.left=toBeInserted;
+                    return true;
+                }
+                else temp=temp.left;
+            } else { //right tree
+                if(temp.right==null)
+                {
+                    temp.right=toBeInserted;
+                    return true;
+                }
+                else temp=temp.right;
+            }
+        }
+        return false;
+    }
+    public void printTree(){
+        if(root!=null)
+            System.out.println(root.value);
+        Node temp=root;
+
+
+    }
+}
