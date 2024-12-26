a4add9d2795635bab8de2f401f1fef4e98e1c882
Ankush7491 <ankushsharma7491@live.com>
2024-12-26 10:41:00 +0530

    Graph Add vortex

---
+++ src/datastructures/graph/GRAPHAPP.java
@@ -0,0 +1,9 @@
+package datastructures.graph;
+
+public class GRAPHAPP {
+    public static void main(String[] args) {
+        Graph graph=new Graph();
+        graph.addVertex("A");
+        graph.printGraph();
+    }
+}
