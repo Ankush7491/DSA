af967d93120269b879a783d7ef5a394d7dac13fe
Ankush7491 <ankushsharma7491@live.com>
2024-12-31 14:09:34 +0530

    kth smallest from last

--- src/datastructures/heap/HEAPAPP.java
+++ src/datastructures/heap/HEAPAPP.java
@@ -1,5 +1,8 @@
 package datastructures.heap;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HEAPAPP {
     public static void main(String[] args) {
         MinHeap heap=new MinHeap();
@@ -10,5 +13,18 @@ public class HEAPAPP {
         heap.insert(100);
         heap.insert(65);
         System.out.println(heap.getHeap());
+    }
+    public static int findKthSmallest(int[] nums,int k){
+        int temp;
+        if(nums.length==1) return nums[0];
+        Heap heap=new Heap();
+        Set<Integer> uniqueValues=new HashSet<>();
+        for(int num:nums){
+            heap.insert(num);
+            if(heap.getHeap().size()>k)
+                heap.remove();
+        }
+        temp=heap.remove();
+       return temp;
     }
 }
