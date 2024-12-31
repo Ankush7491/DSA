7d42c79367c8505db97ce4d87a9db0b2902f5bf6
Ankush7491 <ankushsharma7491@live.com>
2024-12-31 16:00:12 +0530

    BST recurssion contains

--- src/datastructures/binarysearchtree/BSTAPP.java
+++ src/datastructures/binarysearchtree/BSTAPP.java
@@ -3,6 +3,13 @@ package datastructures.binarysearchtree;
 public class BSTAPP {
     public static void main(String[] args) {
         BinarySearchTree binarySearchTree=new BinarySearchTree();
-
+        binarySearchTree.insert(67);
+        binarySearchTree.insert(56);
+        binarySearchTree.insert(80);
+        binarySearchTree.insert(100);
+        binarySearchTree.insert(32);
+        binarySearchTree.printTree();
+        System.out.println(binarySearchTree.contains(100));
+        System.out.println(binarySearchTree.contains(67));
     }
 }
--- src/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/datastructures/binarysearchtree/BinarySearchTree.java
@@ -64,4 +64,17 @@ public class BinarySearchTree {
 
 
     }
+    public boolean rContains(int value){
+        return rContains(root,value);
+    }
+
+    private boolean rContains(Node currentNode, int value) {
+        if(currentNode==null) return false;
+        if(currentNode.value==value) return true;
+        if(value<currentNode.value){
+            return rContains(currentNode.left,value);
+        }else {
+            return rContains(currentNode.right,value);
+        }
+    }
 }
