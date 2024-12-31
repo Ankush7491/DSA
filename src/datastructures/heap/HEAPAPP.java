e2b2f07e86366b7544a8c0f894ab80f6aa378a4f
Ankush7491 <ankushsharma7491@live.com>
2024-12-31 14:24:28 +0530

    kth smallest from last

--- src/datastructures/heap/HEAPAPP.java
+++ src/datastructures/heap/HEAPAPP.java
@@ -1,6 +1,8 @@
 package datastructures.heap;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class HEAPAPP {
@@ -26,5 +28,15 @@ public class HEAPAPP {
         }
         temp=heap.remove();
        return temp;
+    }
+    public static List<Integer> streamMax(int[] nums){
+        List<Integer> maxList=new ArrayList<>(nums.length);
+        if(nums.length==0)return maxList;
+        Heap heap=new Heap();
+        for(int num:nums){
+                heap.insert(num);
+                maxList.add(heap.getHeap().get(0));
+            }
+        return maxList;
     }
 }
