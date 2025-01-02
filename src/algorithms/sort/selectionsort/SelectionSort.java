e7ddb3145af85dcab6d4c559a3e5e28236887872
Ankush7491 <ankushsharma7491@live.com>
2025-01-02 13:07:40 +0530

    selection sort

---
+++ src/algorithms/sort/selectionsort/SelectionSort.java
@@ -0,0 +1,27 @@
+package algorithms.sort.selectionsort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] nums){
+
+        for (int i= 0; i <nums.length; i++) {
+            int min=i;
+            for (int j = i; j <nums.length ; j++) {
+                if(nums[j]<nums[min]){
+                    min=j;
+                }
+            }
+            int current=nums[i];
+            nums[i]=nums[min];
+            nums[min]=current;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = new int[]{9,5,6,3,2,1};
+        selectionSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
