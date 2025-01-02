567e98f233b1010296e3d01b255364d66c0feb66
Ankush7491 <ankushsharma7491@live.com>
2025-01-02 13:08:53 +0530

    selection sort

--- src/algorithms/sort/selectionsort/SelectionSort.java
+++ src/algorithms/sort/selectionsort/SelectionSort.java
@@ -8,7 +8,7 @@ public class SelectionSort {
 
         for (int i= 0; i <nums.length; i++) {
             int min=i;
-            for (int j = i; j <nums.length ; j++) {
+            for (int j = i+1; j <nums.length ; j++) {
                 if(nums[j]<nums[min]){
                     min=j;
                 }
