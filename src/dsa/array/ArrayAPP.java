6f744d42b9f38626682c7b39390a32bd77cf822d
Ankush7491 <ankushsharma7491@live.com>
2025-01-15 09:10:27 +0530

    DSA 2D Array traversal

--- src/dsa/array/ArrayAPP.java
+++ src/dsa/array/ArrayAPP.java
@@ -3,9 +3,12 @@ package dsa.array;
 public class ArrayAPP {
     public static void main(String[] args) {
        TwoDimensionalArray twoDimensionalArray=new TwoDimensionalArray(3,3);
-       twoDimensionalArray.traverse();
-
-
+        twoDimensionalArray.insert(0,1,78);
+        twoDimensionalArray.insert(0,0,70);
+        twoDimensionalArray.insert(0,1,78);
+        twoDimensionalArray.insert(2,1,90);
+        twoDimensionalArray.insert(2,2,96);
+        twoDimensionalArray.traverse();
         SingleDimensionalArray singleDimensionalArray=new SingleDimensionalArray(9);
         singleDimensionalArray.insert(7,90);
         singleDimensionalArray.traverse();
--- src/dsa/array/TwoDimensionalArray.java
+++ src/dsa/array/TwoDimensionalArray.java
@@ -17,11 +17,16 @@ public class TwoDimensionalArray {
     public void traverse(){
         System.out.println(Arrays.deepToString(twoArray));
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < twoArray.length; i++) {
+            for (int j = 0; j < twoArray[i].length; j++) {
                 System.out.print(twoArray[i][j]+ " ||");
             }
             System.out.println("row "+ i);
         }
+    }
+    public void insert(int row,int column,int value){
+        if(twoArray[row][column]==Integer.MIN_VALUE)
+            twoArray[row][column]=value;
+        else System.out.println("Already Occupied,cannot insert");
     }
 }
