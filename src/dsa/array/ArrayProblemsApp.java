cd0f7c913d7dc224dddd0b7c3ecdcc4775fff7e7
Ankush7491 <ankushsharma7491@live.com>
2025-01-15 15:02:15 +0530

    optimized FirstTopTwoScores

--- src/dsa/array/ArrayProblemsApp.java
+++ src/dsa/array/ArrayProblemsApp.java
@@ -15,7 +15,11 @@ public class ArrayProblemsApp {
         int[][] array={{1,2,3},{4,5,6},{7,8,9}};
         Assertions.assertEquals(15,sumDiagonalElements(array));
     }
-
+    @Test
+    public void testTopTwoScores(){
+        int[] array={84,85,86,87,85,90,85,83,23,45,84,1,2,0};
+        Assertions.assertArrayEquals(new int[]{90,87},findTopTwoScores(array));
+    }
     public static void testMiddle(){
         int[] array={1,2,5,8,9};
         int[] array2={1};
@@ -37,5 +41,37 @@ public class ArrayProblemsApp {
                         sum+=array[i][i];
         }
         return sum;
+    }
+    public static int[] findTopTwoScores(int[] array){
+        if(array.length<2) return array;
+        int[] topScores=new int[2];
+        int index=0;
+        while(index<array.length){
+            if(array[index]>topScores[0])
+            topScores[0]=array[index];
+            index++;
+        }
+        index=0;
+        while(index<array.length){
+            if(array[index]!=topScores[0]&&array[index]>topScores[1])
+                topScores[1]=array[index];
+            index++;
+        }
+        return topScores;
+    }
+    public static int[] findTopTwoScoresOptimized(int[] array) {
+        int firstHighest = Integer.MIN_VALUE;
+        int secondHighest = Integer.MIN_VALUE;
+
+        for (int score : array) {
+            if (score > firstHighest) {
+                secondHighest = firstHighest;
+                firstHighest = score;
+            } else if (score > secondHighest && score < firstHighest) {
+                secondHighest = score;
+            }
+        }
+
+        return new int[]{firstHighest, secondHighest};
     }
 }
