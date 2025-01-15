412c6814fc0e95624c57e2617ec80087885c1f28
Ankush7491 <ankushsharma7491@live.com>
2025-01-15 14:21:29 +0530

    optimized sumDiagonalElements

--- src/dsa/array/ArrayProblemsApp.java
+++ src/dsa/array/ArrayProblemsApp.java
@@ -1,9 +1,22 @@
 package dsa.array;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 
 public class ArrayProblemsApp {
     public static void main(String[] args) {
+
+    }
+
+    @Test
+    public  void testSumDiagonalElements() {
+        int[][] array={{1,2,3},{4,5,6},{7,8,9}};
+        Assertions.assertEquals(15,sumDiagonalElements(array));
+    }
+
+    public static void testMiddle(){
         int[] array={1,2,5,8,9};
         int[] array2={1};
         System.out.println(Arrays.toString(middle(array)));
@@ -15,5 +28,14 @@ public class ArrayProblemsApp {
             newArray[i-1]=array[i];
         }
         return newArray;
+    }
+    public static int sumDiagonalElements(int[][] array) {
+        if(array.length==0) return 0;
+        if(array[0].length==0) return 0;
+        int sum=0;
+        for (int i = 0; i < array.length; i++) {
+                        sum+=array[i][i];
+        }
+        return sum;
     }
 }
